b856f91d0fcde135a9ca7db86f5c4fd9e606858e
D1353L <meghanik400@gmail.com>
2017-10-12 15:41:40 +0300

    Unused url-param for post route deleted

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get 'matches/:date', to: 'matches#index'
-      post 'matches/refresh/:date', to: 'matches#refresh'
+      post 'matches/refresh', to: 'matches#refresh'
     end
   end
 end
